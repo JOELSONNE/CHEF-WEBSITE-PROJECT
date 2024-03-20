0575e1a5fe99f32fb333b3da71896969cad947ed
JOELSONNE <157706963+JOELSONNE@users.noreply.github.com>
2024-03-20 18:00:27 +0000

    registration

--- HTML/main.js
+++ HTML/main.js
@@ -1,14 +1,13 @@
-const menuIcon = document.getElementById("menu-icon");
-const slideoutMenu = document.getElementById("slideout-menu");
-var searchBox = document.getElementById("searchbox");
+// Get references to the search icon and search box elements
+
 
 var searchIcon = document.getElementById('Search-icon');
+var searchBox = document.getElementById('searchbox');
 
-
-
-searchIcon.addEventListener('click', function () {
-
-    if (searchBox.style.display === 'none' || searchBox.style.display === '') {
+// Add a click event listener to the search icon
+searchIcon.addEventListener('click', function() {
+    // Toggle the visibility of the search box
+    if (searchBox.offsetHeight === 0) {
         searchBox.style.display = 'block';
         searchBox.style.top = '70px';
         searchBox.style.pointerEvents = 'auto';
@@ -19,3 +18,16 @@ searchIcon.addEventListener('click', function () {
     }
 });
 
+// Get references to the menu icon and slideout menu elements
+var menuIcon = document.getElementById("menu-icon");
+var slideoutMenu = document.getElementById("slideout-menu");
+
+// Add a click event listener to the menu icon
+menuIcon.addEventListener('click', function() {
+    // Toggle the visibility of the slideout menu
+    if (slideoutMenu.style.display === 'none' || slideoutMenu.style.display === '') {
+        slideoutMenu.style.display = 'block';
+    } else {
+        slideoutMenu.style.display = 'none';
+    }
+});
