5ca7f2f4b2e34d4ad87f7d73d873acd6c3b6f8c2
JOELSONNE <157706963+JOELSONNE@users.noreply.github.com>
2024-03-22 19:10:06 +0000

    head ache

--- HTML/JAVA/registration.js
+++ HTML/JAVA/registration.js
@@ -1,43 +1,26 @@
-function cambiar_login(){
-    document.querySelector('cont_forms').className = "cont_forms cont_forms_active_login";
+function cambiar_login() {
+    document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
     document.querySelector('.form_login').style.display = "block";
+    document.querySelector('.form_login').style.opacity = "1";
     document.querySelector('.form_sign_up').style.opacity = "0"; 
-
-    setTimeout(function(){  document.querySelector('.form_login').style.opacity = "1"; },400);  
-    
-    setTimeout(function(){    
     document.querySelector('.form_sign_up').style.display = "none";
-    },200); 
 }
-       
 
-
-  
-
-function cambiar_sign_up(at) {
+function cambiar_sign_up() {
     document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
     document.querySelector('.form_sign_up').style.display = "block";
+    document.querySelector('.form_sign_up').style.opacity = "1";
     document.querySelector('.form_login').style.opacity = "0";
-    
-    setTimeout(function(){  document.querySelector('.form_sign_up').style.opacity = "1";
-    },100);  
-
-    setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
-    },400);  
-
-}    
-
-
+    document.querySelector('.form_login').style.display = "none";
+}
 
 function ocultar_login_sign_up() {
-
     document.querySelector('.cont_forms').className = "cont_forms";  
     document.querySelector('.form_sign_up').style.opacity = "0";               
     document.querySelector('.form_login').style.opacity = "0"; 
 
-    setTimeout(function(){
-    document.querySelector('.form_sign_up').style.display = "none";
-    document.querySelector('.form_login').style.display = "none";
-    },500);  
-  
+    setTimeout(function() {
+        document.querySelector('.form_sign_up').style.display = "none";
+        document.querySelector('.form_login').style.display = "none";
+    }, 500);  
 }
