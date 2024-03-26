89d14bc76bd0375f3691212efe2faf68447ddd56
JOELSONNE <157706963+JOELSONNE@users.noreply.github.com>
2024-03-26 02:06:07 +0000

    php validate

---
+++ HTML/PHP/register.php
@@ -0,0 +1,20 @@
+<?php
+include 'conncection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = $_POST['email'];
+    $username = $_POST['Username'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO users (email, username, password) VALUES ('$email', '$username', '$password')";
+
+    if ($conn->quenry($sql) ===TRUE){
+
+        header("Location: login.php");
+    } else {
+        echo "Error:" . $sql . "<br>" . $conn->error;
+    }
+    
+
+
+}
